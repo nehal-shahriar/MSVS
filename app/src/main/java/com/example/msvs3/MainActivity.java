ae9a783440b60b299e09bb1870c67c9d0616e543
rifat <rifatfahmida00@gmail.com>
2020-03-10 14:55:02 -0700

    Barcode

--- app/src/main/java/com/example/msvs3/MainActivity.java
+++ app/src/main/java/com/example/msvs3/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.msvs3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 
 public class MainActivity extends AppCompatActivity {
     Button userbtn,adminbtn;
+    Button regbtn;
     private ZXingScannerView scannerView;
     DatabaseReference dref;
 
@@ -22,9 +30,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dref= FirebaseDatabase.getInstance().getReference("Result1");
+        dref= FirebaseDatabase.getInstance().getReference("Barcode");
 
         userbtn=findViewById(R.id.userbtn);
+        regbtn=findViewById(R.id.regbtn);
         adminbtn=findViewById((R.id.adminbtn));
         userbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,22 +50,69 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
     }
-    public void scancode(View view){
+    public void scanCode(View view){
         scannerView = new ZXingScannerView(this);
         scannerView.setResultHandler(new ZXingScannerResultHandler());
+        setContentView(scannerView);
+        scannerView.startCamera();
+
     }
     @Override
     public void onPause(){
         super.onPause();
         scannerView.stopCamera();
+
     }
-    class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler{
+
+
+
+
+    class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler {
+
         @Override
-        public void handleResult(com.google.zxing.Result result){
-            String resultCode= result.getText();
-            String key=dref.push().getKey();
-            dref.child(key).setValue(resultCode);
+        public void handleResult(Result result) {
+            //dref= new Firebase(Config.https://console.firebase.google.com/u/1/project/diabeticdiary-ebc55/database/diabeticdiary-ebc55/data)
+            final String resultCode = result.getText();
+            DatabaseReference usersRef ;
+            usersRef = FirebaseDatabase.getInstance().getReference().child("Info");
+
+            //usersRef.push().child("value").setValue(resultCode);
+
+
+
+            usersRef.orderByChild("value").equalTo(resultCode).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    int flag=0;
+                    for(DataSnapshot data: dataSnapshot.getChildren()) {
+                        if (!dataSnapshot.exists()) {
+
+                            Toast.makeText(MainActivity.this, "Not valid", Toast.LENGTH_SHORT).show();
+                        } else {
+
+
+                            flag=1;
+                        }
+                    }
+                    if(flag==0)
+                    {
+                        Toast.makeText(MainActivity.this, "Not valid", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "valid barcode", Toast.LENGTH_SHORT).show();
+                        Intent intent= new Intent(MainActivity.this,user.class);
+                        startActivity(intent);
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
 
             setContentView(R.layout.activity_main);
             scannerView.stopCamera();
